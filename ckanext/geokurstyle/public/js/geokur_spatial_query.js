9f0ccc3d17adb28d0e3b0dc2908169aca45b4eba
rue_a <arne.ruemmler@gmail.com>
2021-02-04 18:17:04 +0100

    ''

--- ckanext/geokurstyle/public/js/geokur_spatial_query.js
+++ ckanext/geokurstyle/public/js/geokur_spatial_query.js
@@ -166,7 +166,7 @@ this.ckan.module('spatial-query', function ($, _) {
             var previous_bbox;
             setPreviousBBBox();
             setPreviousExtent();
-            map.setZoom(3);
+            map.setView([0, 20], 3);
 
             // OK, when we expand we shouldn't zoom then
             map.on('zoomstart', function (e) {
