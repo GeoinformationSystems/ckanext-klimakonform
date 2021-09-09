750f8fea243dc31e8280a6a1159d5c6454d36747
pthiemt <peggy.thiemt@tu-dresden.de>
2021-09-09 12:39:27 +0200

    fix for legal notice and about pages

--- ckanext/klimakonform/plugin.py
+++ ckanext/klimakonform/plugin.py
@@ -4,6 +4,7 @@ import ckan.plugins.toolkit as toolkit
 
 class KlimakonformPlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IConfigurer)
+    plugins.implements(plugins.IRoutes)
 
     # IConfigurer
 
@@ -11,3 +12,15 @@ class KlimakonformPlugin(plugins.SingletonPlugin):
         toolkit.add_template_directory(config_, 'templates')
         toolkit.add_public_directory(config_, 'public')
         toolkit.add_resource('public', 'ckanext-klimakonform')
+    
+    # IRoutes
+    
+    def before_map(self, map):
+
+        return map
+
+    def after_map(self, map):        
+        map.connect('legal_notice', '/legal-notice',
+                    controller='ckanext.klimakonform.controller:KlimakonformController',
+                    action='legal_notice')
+        return map
