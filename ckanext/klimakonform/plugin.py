add6d6b3ade8cd6f995d81a3032bf583a8c5b33f
simeonwetzel <simeon.wetzel@tu-dresden.de>
2022-03-03 11:07:28 +0100

    Added a spatial search feature. Implemented automatic geocoding of search-query. Filtering search results if coordinates are found. Nominatim (OSM-based) with geopy was used as geocoding API. Further work needed because geocoded results must be cached... see https://operations.osmfoundation.org/policies/nominatim/

--- ckanext/klimakonform/plugin.py
+++ ckanext/klimakonform/plugin.py
@@ -1,7 +1,25 @@
+# -*- coding: utf-8 -*-
 import ckan.plugins as plugins
+from ckanext.discovery.plugins.search_suggestions.interfaces import \
+    ISearchTermPreprocessor
+import ckantoolkit as tk
+from ckanext.spatial.plugin.__init__ import SpatialQuery
 import ckan.plugins.toolkit as toolkit
 from helpers import update_resources, set_of_time_periods, set_of_seasons, set_of_themes
 import yaml
+from geopy.geocoders import Nominatim
+import logging
+
+log = logging.getLogger(__name__)
+log.setLevel(logging.DEBUG)
+log.addHandler(logging.StreamHandler())
+
+config = tk.config
+
+
+
+
+geolocator = Nominatim(user_agent="http")
 
 def load_rekis_file_yaml_to_dict():
     # TODO: change to relative path
@@ -29,6 +47,10 @@ class KlimakonformPlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IConfigurer)
     plugins.implements(plugins.IRoutes)
     plugins.implements(plugins.ITemplateHelpers)
+    plugins.implements(ISearchTermPreprocessor)
+    plugins.implements(plugins.IPackageController, inherit=True)
+    plugins.implements(SpatialQuery)
+
     # IConfigurer
 
     def update_config(self, config_):
@@ -61,4 +83,61 @@ class KlimakonformPlugin(plugins.SingletonPlugin):
                     action='legal_notice')
         return map
 
+    
+    #Spatial Query Implementation
+
+    def before_search(self, search_params):
+        '''
+        This function geocodes every search-query and adds it as additional query-param
+        before passing the parameter to the search interfaces
+        '''
+
+        #Todo: add caching of geocoded results
+        context = SpatialQuery()       
+        
+        if 'q' in search_params:
+            log.debug('Looking for geolocation from search query')
+            log.debug('org_search_params = {}'.format(search_params))
+
+            try:
+                query_geocoded = geolocator.geocode(search_params['q'])
+
+                if query_geocoded is not None:
+                    coords = '{0}+{1}'.format(query_geocoded.latitude, query_geocoded.longitude)
+
+                    bbox = '{0},{1},{0},{1}'.format(query_geocoded.longitude, query_geocoded.latitude)
+                    log.debug('Found following coordinates and appended it to query: {}'.format(coords))
+
+                    search_params['extras']['ext_bbox'] = bbox
+                    search_params['extras']['ext_prev_extent'] = bbox
+                    search_params['q'] = u''
+                    search_params_filtered = SpatialQuery.before_search(context, search_params)
+                    log.debug('search_params_filtered_with_bbox : {}'.format(search_params))
+                    return search_params_filtered
+            except Exception as e: 
+                log.debug('Failed to find geolocation because {}'.format(e))
+        
+        return search_params
+    
+
+    # ISearchTermPreprocessor
+
+    def preprocess_search_term(self, term):
+        '''
+        Preprocess and filter a search term.
+
+        ``term`` is a search term extracted from a user's search query.
+
+        If this method returns a false value then the term is ignored
+        w.r.t. search suggestions. This is useful for filtering stop
+        words and unwelcome content.
+
+        Otherwise the return value of the method is used instead of the
+        original search term. In most cases you simply return the value
+        unchanged.
+
+        Note that all of this only affects the generation of the search
+        suggestions but not the search itself.
+        '''
+        return term    
     
